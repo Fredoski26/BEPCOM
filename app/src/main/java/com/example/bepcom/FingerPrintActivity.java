6b64168c719117a80f0f3b6ae9de91e8636370fa
Esemudia <57021444+Esemudia@users.noreply.github.com>
2023-06-20 15:08:34 +0100

    Add files via upload

--- app/src/main/java/com/example/bepcom/FingerPrintActivity.java
+++ app/src/main/java/com/example/bepcom/FingerPrintActivity.java
@@ -5,12 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.suprema.BioMiniFactory;
+import com.suprema.CaptureResponder;
+import com.suprema.IBioMiniDevice;
+import com.suprema.IUsbEventHandler;
 
 public class FingerPrintActivity extends AppCompatActivity {
- AppCompatButton exit;
+    AppCompatButton exit;
+    private BioMiniFactory mBioMiniFactory=null;
+    private IBioMiniDevice mCurrentDevice=null;
+
+    ImageView right_thumb;
+    ImageView Left_thumb;
+    ImageView right_index;
+    ImageView left_index;
+
+    TextView right_thumb_text;
+    TextView left_thumb_text;
+    TextView right_index_text;
+    TextView left_index_text;
+    private FingerPrintActivity mainContext;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -22,12 +44,263 @@ public class FingerPrintActivity extends AppCompatActivity {
     }
 
     private void inits() {
-        exit = findViewById(R.id.exit);
+        exit = (AppCompatButton) findViewById(R.id.exit);
+        right_thumb_text= (TextView) findViewById(R.id.right_thumb_text);
+        left_thumb_text= (TextView) findViewById(R.id.Left_thumb_text);
+        left_index_text= (TextView) findViewById(R.id.left_index_text);
+        right_index_text= (TextView) findViewById(R.id.right_index_text);
+        mainContext=this;
+        boolean b = false;
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        right_thumb_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                right_thumb= (ImageView) findViewById(R.id.right_Thumb);
+                mBioMiniFactory = new BioMiniFactory(mainContext){
+                    @Override
+                    public void onDeviceChange(IUsbEventHandler.DeviceChangeEvent event, Object dev)
+                    {
+                        if (event== IUsbEventHandler.DeviceChangeEvent.DEVICE_ATTACHED && mCurrentDevice==null)
+                        {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
 
-        exit.setOnClickListener(v -> onBackPressed());
+                                    int cnt=0;
+                                    while (mBioMiniFactory==null && cnt<20){
+                                        SystemClock.sleep(1000);
+                                        cnt++;
+                                    }
+                                    if (mBioMiniFactory!=null)
+                                    {
+                                        mCurrentDevice=mBioMiniFactory.getDevice(0);
+                                    }
+                                }
+                            }).start();
+                        }
+
+                        IBioMiniDevice.CaptureOption captureOption= new IBioMiniDevice.CaptureOption();
+                        captureOption.captureTemplate=true;
+
+                        mCurrentDevice.captureSingle(captureOption, new CaptureResponder() {
+                            @Override
+                            public boolean onCaptureEx(final Object o, final Bitmap bitmap, final IBioMiniDevice.TemplateData templateData, final IBioMiniDevice.FingerState fingerState) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (bitmap != null) {
+                                            ImageView iv = findViewById(R.id.right_Thumb);
+                                            if (iv != null) {
+                                                iv.setImageBitmap(bitmap);
+                                            }
+                                        }
+                                    }
+                                });
+                                if (templateData!=null)
+                                {
+
+                                }
+                                else if
+                                (mCurrentDevice != null && event == IUsbEventHandler.DeviceChangeEvent.DEVICE_DETACHED && mCurrentDevice.isEqual(dev))
+                                {  mCurrentDevice = null; }
+
+                                ;
+                                return false;
+                            }
+
+                        }, false);
+                    }
+                };
+
+            }
+        });
+
+        left_thumb_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                right_thumb= (ImageView) findViewById(R.id.right_Thumb);
+                mBioMiniFactory = new BioMiniFactory(mainContext){
+                    @Override
+                    public void onDeviceChange(IUsbEventHandler.DeviceChangeEvent event, Object dev)
+                    {
+                        if (event== IUsbEventHandler.DeviceChangeEvent.DEVICE_ATTACHED && mCurrentDevice==null)
+                        {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    int cnt=0;
+                                    while (mBioMiniFactory==null && cnt<20){
+                                        SystemClock.sleep(1000);
+                                        cnt++;
+                                    }
+                                    if (mBioMiniFactory!=null)
+                                    {
+                                        mCurrentDevice=mBioMiniFactory.getDevice(0);
+                                    }
+                                }
+                            }).start();
+                        }
 
 
+                        IBioMiniDevice.CaptureOption captureOption= new IBioMiniDevice.CaptureOption();
+                        captureOption.captureTemplate=true;
+
+                        mCurrentDevice.captureSingle(captureOption, new CaptureResponder() {
+                            @Override
+                            public boolean onCaptureEx(final Object o,final Bitmap bitmap,final IBioMiniDevice.TemplateData templateData,final IBioMiniDevice.FingerState fingerState) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (bitmap != null) {
+                                            ImageView iv = findViewById(R.id.right_Thumb);
+                                            if (iv != null) {
+                                                iv.setImageBitmap(bitmap);
+                                            }
+                                        }
+                                    }
+                                });
+                                if (templateData!=null)
+                                {
+
+                                }
+                                else if
+                                (mCurrentDevice != null && event == IUsbEventHandler.DeviceChangeEvent.DEVICE_DETACHED && mCurrentDevice.isEqual(dev))
+                                {  mCurrentDevice = null; }
+                                return false;
+                            }
+
+                        },false);
+                    }
+                };
+            }
+        });
+
+        right_index_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                right_thumb= (ImageView) findViewById(R.id.right_Thumb);
+                mBioMiniFactory = new BioMiniFactory(mainContext){
+                    @Override
+                    public void onDeviceChange(IUsbEventHandler.DeviceChangeEvent event, Object dev)
+                    {
+                        if (event== IUsbEventHandler.DeviceChangeEvent.DEVICE_ATTACHED && mCurrentDevice==null)
+                        {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    int cnt=0;
+                                    while (mBioMiniFactory==null && cnt<20){
+                                        SystemClock.sleep(1000);
+                                        cnt++;
+                                    }
+                                    if (mBioMiniFactory!=null)
+                                    {
+                                        mCurrentDevice=mBioMiniFactory.getDevice(0);
+                                    }
+                                }
+                            }).start();
+                        }
+
+                        IBioMiniDevice.CaptureOption captureOption= new IBioMiniDevice.CaptureOption();
+                        captureOption.captureTemplate=true;
+
+                        mCurrentDevice.captureSingle(captureOption, new CaptureResponder() {
+                            @Override
+                            public boolean onCaptureEx(final Object o,final Bitmap bitmap,final IBioMiniDevice.TemplateData templateData,final IBioMiniDevice.FingerState fingerState) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (bitmap != null) {
+                                            ImageView iv = findViewById(R.id.right_Thumb);
+                                            if (iv != null) {
+                                                iv.setImageBitmap(bitmap);
+                                            }
+                                        }
+                                    }
+                                });
+                                 if (templateData!=null)
+                                {
+
+                                }
+                                else if
+                                (mCurrentDevice != null && event == IUsbEventHandler.DeviceChangeEvent.DEVICE_DETACHED && mCurrentDevice.isEqual(dev))
+                                {  mCurrentDevice = null; }
+                                return false;
+                            }
+
+                        }, true);
+                    }
+                };
+            }
+        });
+
+        left_index_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                right_thumb= (ImageView) findViewById(R.id.right_Thumb);
+                mBioMiniFactory = new BioMiniFactory(mainContext){
+                    @Override
+                    public void onDeviceChange(IUsbEventHandler.DeviceChangeEvent event, Object dev)
+                    {
+                        if (event== IUsbEventHandler.DeviceChangeEvent.DEVICE_ATTACHED && mCurrentDevice==null)
+                        {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    int cnt=0;
+                                    while (mBioMiniFactory==null && cnt<20){
+                                        SystemClock.sleep(1000);
+                                        cnt++;
+                                    }
+                                    if (mBioMiniFactory!=null)
+                                    {
+                                        mCurrentDevice=mBioMiniFactory.getDevice(0);
+                                    }
+                                }
+                            }).start();
+                        }
+
+                        IBioMiniDevice.CaptureOption captureOption= new IBioMiniDevice.CaptureOption();
+                        captureOption.captureTemplate=true;
+
+                        mCurrentDevice.captureSingle(captureOption,new CaptureResponder() {
+                            @Override
+                            public boolean onCaptureEx(final Object o,final Bitmap bitmap,final IBioMiniDevice.TemplateData templateData,final IBioMiniDevice.FingerState fingerState) {
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (bitmap != null) {
+                                            ImageView iv = findViewById(R.id.right_Thumb);
+                                            if (iv != null) {
+                                                iv.setImageBitmap(bitmap);
+                                            }
+                                        }
+                                    }
+                                });
+                                if (templateData!=null)
+                                {
+
+                                }
+                                else if
+                                (mCurrentDevice != null && event == IUsbEventHandler.DeviceChangeEvent.DEVICE_DETACHED && mCurrentDevice.isEqual(dev))
+                                {  mCurrentDevice = null; }
+                                return false;
+
+                            }
+
+                        }, true);
+                    }
+                };
+            }
+        });
     }
-
-
 }
--- app/src/main/java/com/example/bepcom/PhotoCaptureActivity.java
+++ app/src/main/java/com/example/bepcom/PhotoCaptureActivity.java
@@ -3,19 +3,30 @@ package com.example.bepcom;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PhotoCaptureActivity extends AppCompatActivity {
-   ImageView userImage;
-   AppCompatButton done;
-   AppCompatButton back;
-   TextView retake;
+    ImageView userImage;
+    AppCompatButton done;
+    AppCompatButton back;
+    TextView retake;
+
+    TextView capture;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +37,11 @@ public class PhotoCaptureActivity extends AppCompatActivity {
     }
 
     private void inits() {
-        userImage = findViewById(R.id.userImage);
-        done = findViewById(R.id.done);
-        back = findViewById(R.id.back);
-        retake = findViewById(R.id.retakeCapture);
+
+        done =  (AppCompatButton) findViewById(R.id.done);
+        back = (AppCompatButton) findViewById(R.id.back);
+        retake = (TextView) findViewById(R.id.retakeCapture);
+        capture = (TextView) findViewById(R.id.takeCapture);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +49,55 @@ public class PhotoCaptureActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(PhotoCaptureActivity.this, android.Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(PhotoCaptureActivity.this, new String[]{android.Manifest.permission.CAMERA},
+                            MY_CAMERA_PERMISSION_CODE);
+                }
+                else {
+                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera_intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+        retake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(PhotoCaptureActivity.this, android.Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(PhotoCaptureActivity.this, new String[]{Manifest.permission.CAMERA},
+                            MY_CAMERA_PERMISSION_CODE);
+                }
+                else {
+                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera_intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            userImage=(ImageView) findViewById(R.id.userImage);
+            done =  (AppCompatButton) findViewById(R.id.done);
+            retake = (TextView) findViewById(R.id.retakeCapture);
+            capture = (TextView) findViewById(R.id.takeCapture);
+            capture.setVisibility(View.INVISIBLE);
+            retake.setVisibility(View.VISIBLE);
+            done.setVisibility(View.VISIBLE);
+            userImage.setImageBitmap(photo);
+        }
     }
 }
